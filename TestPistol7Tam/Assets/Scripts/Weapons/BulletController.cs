27f8c9918a10ded719c44a014937dded336d3ee6
faAanya <108868725+faAanya@users.noreply.github.com>
2024-07-23 20:15:23 +0300

    Shooting to closet interactable object

---
+++ TestPistol7Tam/Assets/Scripts/Weapons/BulletController.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletController : MonoBehaviour
+{
+    [SerializeField] private Rigidbody2D bulletRB;
+
+}
---
+++ TestPistol7Tam/Assets/WeaponClass.cs
@@ -0,0 +1,57 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WeaponClass : MonoBehaviour, WeaponInterface
+{
+
+    public bool isActiveWeapon = true;
+    public GameObject bullet;
+    public int amountOfBullets;
+
+    public int damage;
+
+    public int bulletSpeed;
+
+    public virtual void Shoot()
+    {
+        // GameObject newBullet = Instantiate(bullet, transform.position, Quaternion.identity);
+        // newBullet.gameObject.GetComponent<Rigidbody2D>().AddForce(FindClosestInteractableObject().gameObject.transform.position, ForceMode2D.Impulse);
+        InteractableObject interactableObject = FindClosestInteractableObject();
+        GameObject newProjectile = Instantiate(bullet, gameObject.transform.position, Quaternion.identity);
+        newProjectile.GetComponent<Rigidbody2D>().velocity = (interactableObject.transform.position + interactableObject.GetComponent<BoxCollider2D>().bounds.size / 2 - gameObject.transform.position) * bulletSpeed;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButton(1) && isActiveWeapon)
+        {
+            Shoot();
+        }
+    }
+
+    public void ShootToClosetEnemy()
+    {
+
+    }
+
+    InteractableObject FindClosestInteractableObject() //looks for closest interactableObject
+    {
+        float distanceToClosestinteractableObject = Mathf.Infinity;
+        InteractableObject closestinteractableObject = null;
+        IEnumerable<InteractableObject> allEnemies = FindObjectsOfType<InteractableObject>();
+
+        foreach (InteractableObject currentinteractableObject in allEnemies)
+        {
+            float distanceTointeractableObject = (currentinteractableObject.transform.position - this.transform.position).sqrMagnitude;
+            if (distanceTointeractableObject < distanceToClosestinteractableObject)
+            {
+                distanceToClosestinteractableObject = distanceTointeractableObject;
+                closestinteractableObject = currentinteractableObject;
+            }
+        }
+        return closestinteractableObject;
+
+    }
+}
